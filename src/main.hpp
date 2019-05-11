aab0a21e4da153b152cc2520bc22d224a70e3d4d
Ales Koblizek <koblizek.ales@seznam.cz>
2019-05-11 08:51:02 +0200

    add missing header, fix build to work with newer glm

---
+++ src/main.hpp
@@ -0,0 +1,10 @@
+#ifndef MAIN_HPP_19_05_11_08_45_02
+#define MAIN_HPP_19_05_11_08_45_02 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#endif /* MAIN_HPP_19_05_11_08_45_02 */
