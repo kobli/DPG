b68a6c1d2f9a96bcbfa6f9dac5d7fdaebb04fdb5
Ales Koblizek <koblizek.ales@seznam.cz>
2019-05-12 12:18:07 +0200

    fix gen_stats_batch, add processing scripts

--- bat/gen_stats_batch.py
+++ bat/gen_stats_batch.py
@@ -4,7 +4,13 @@ executable = "Release\FrustumCulling.exe"
 statsFilePath = "../stats/"
 
 viewNames = ["A10.view", "city_B.view", "conference_B.view", "teapots_B.view", "asianDragon_B.view", "fforest_B.view", "city2_B.view"]
-noOptimFlags = ["", "-no-cotant-test", "-no-plane-test", "-no-plane-masking", "-no-cotant-test -no-plane-test -no-plane-masking"]
+noOptimFlags = [
+        "-no-octant-test -no-plane-coherency -no-plane-masking",
+        "-no-plane-coherency -no-plane-masking",
+        "-no-octant-test -no-plane-masking",
+        "-no-octant-test -no-plane-coherency",
+        "",
+        ]
 noOptimFileSuffixes = ["", "o", "l", "m", "olm"]
 
 for viewFileName in viewNames:
