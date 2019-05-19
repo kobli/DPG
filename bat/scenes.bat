c58701b3727c8438cee3d942c9d432aa22bb1ae6
Koblizek <koblial2@cvut.cz>
2019-05-19 10:15:03 +0200

    add camroutes and stats (culling, no culling) for all scenes

---
+++ bat/scenes.bat
@@ -0,0 +1,14 @@
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-plane-coherency -s scenes/asianDragon.obj -p ../stats/asianDragon.camroute -m ../stats/asianDragon_10000_com.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-plane-coherency -s scenes/block_in_pompeii.high_lod_combined.obj -p ../stats/block_in_pompeii.high_lod_combined.camroute -m ../stats/block_in_pompeii.high_lod_combined_10000_com.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-plane-coherency -s scenes/City4M.obj -p ../stats/City4M.camroute -m ../stats/City4M_10000_com.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-plane-coherency -s scenes/part_of_pompeii.01.final.combined.obj -p ../stats/part_of_pompeii.01.final.combined.camroute -m ../stats/part_of_pompeii.01.final.combined_10000_com.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-plane-coherency -s scenes/PowerPlantM.obj -p ../stats/PowerPlantM.camroute -m ../stats/PowerPlantM_10000_com.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-plane-coherency -s scenes/ten_blocks_in_pompeii.01.final.combined.obj -p ../stats/ten_blocks_in_pompeii.01.final.combined.camroute -m ../stats/ten_blocks_in_pompeii.01.final.combined_10000_com.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-plane-coherency -s scenes/vienna_cropped.obj -p ../stats/vienna_cropped.camroute -m ../stats/vienna_cropped_10000_com.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-frustum-culling -s scenes/asianDragon.obj -p ../stats/asianDragon.camroute -m ../stats/asianDragon.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-frustum-culling -s scenes/block_in_pompeii.high_lod_combined.obj -p ../stats/block_in_pompeii.high_lod_combined.camroute -m ../stats/block_in_pompeii.high_lod_combined.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-frustum-culling -s scenes/City4M.obj -p ../stats/City4M.camroute -m ../stats/City4M.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-frustum-culling -s scenes/part_of_pompeii.01.final.combined.obj -p ../stats/part_of_pompeii.01.final.combined.camroute -m ../stats/part_of_pompeii.01.final.combined.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-frustum-culling -s scenes/PowerPlantM.obj -p ../stats/PowerPlantM.camroute -m ../stats/PowerPlantM.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-frustum-culling -s scenes/ten_blocks_in_pompeii.01.final.combined.obj -p ../stats/ten_blocks_in_pompeii.01.final.combined.camroute -m ../stats/ten_blocks_in_pompeii.01.final.combined.stats
+Release\FrustumCulling.exe -c 10000 -q -u 0.001 -no-frustum-culling -s scenes/vienna_cropped.obj -p ../stats/vienna_cropped.camroute -m ../stats/vienna_cropped.stats
